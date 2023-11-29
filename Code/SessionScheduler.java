f096910152ce67a55019b16b91860bd07ba779f5
AvgProgrammer <katsaros.lampros01@gmail.com>
2023-11-29 12:06:26 +0200

    Add the SessionScheduler.java

---
+++ Code/SessionScheduler.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.time;
+
+public class SessionScheduler {
+    private String sender,description,location,time;
+    private List<String> Recipients=new ArrayList<String>();
+}
