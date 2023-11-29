fbc77bbb253c483b312a298b7e7459cdd11b0fdf
AvgProgrammer <katsaros.lampros01@gmail.com>
2023-11-29 12:09:48 +0200

    Add the basic methods to SessionScheduler.java

--- Code/SessionScheduler.java
+++ Code/SessionScheduler.java
@@ -1,7 +1,29 @@
 import java.util.ArrayList;
 import java.time;
+import java.sql.Time;
 
 public class SessionScheduler {
     private String sender,description,location,time;
     private List<String> Recipients=new ArrayList<String>();
+
+    public void getSender(String Sender){
+        this.sender=Sender;
+    }
+    public void getRecipents(){
+    }
+    public void getDescription(String Description){
+        this.description=Description;
+    }
+    public void getDate(){
+
+    }
+    public void getLocation(String Location){
+        this.location=Location;
+    }
+    public void getTime(String Time){
+        this.time=Time;
+    }
+    public void sendEmail(){
+
+    }
 }
