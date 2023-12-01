8a69b135217f5eceac3501c2a023f06a14175b1c
AvgProgrammer <katsaros.lampros01@gmail.com>
2023-12-01 18:50:10 +0200

    Add the Player.java and change the PlayerMVP

---
+++ Code/Player.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private String Name,Email,Code;
+    private list<CharacterSheet> Characters=new Arraylist();
+    private list<SessionScheduler> Sessions=new Arraylist();
+    
+    public void SetName(String name){
+        this.Name=name;
+    }
+    public void SetCode(String code){
+        this.Code=code;
+    }
+    public void SetEmail(String email){
+        this.Email=email;
+    }
+
+    public String GetName(){
+        return Name;
+    }
+    public String GetEmail(){
+        return Email;
+    }
+    public String GetNCode(){
+        return Code;
+    }
+
+    public void AddCharacter(){
+        
+    }
+    public void AddSession(){
+        
+    }
+}
+
