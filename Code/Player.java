a991ed2b4da1f60ad05ea2fa43c7dab02f5fc5f1
tutorial <dioni.ahmetaj@hotmail.com>
2023-12-03 12:58:40 +0200

    few changes on Player.java & CharacterSheetMVP.java & sequence-CharacterSheetMVP.uxf

--- Code/Player.java
+++ Code/Player.java
@@ -2,35 +2,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private String Name,Email,Code;
-    private list<CharacterSheet> Characters=new Arraylist();
-    private List<SessionScheduler> Sessions=new Arraylist();
-    
-    public void SetName(String name){
-        this.Name=name;
-    }
-    public void SetCode(String code){
-        this.Code=code;
-    }
-    public void SetEmail(String email){
-        this.Email=email;
+
+    private String Name, Email, Password;
+    private List<CharacterSheet> Characters = new Arraylist();
+    private List<SessionScheduler> Sessions = new Arraylist();
+
+    public Player(String Name, String Email, String Password, List<CharacterSheet> Characters,
+            List<SessionScheduler> Sessions) {
+        Name = Email = Password = "";
+        Characters = Sessions = null;
     }
 
-    public String GetName(){
+    public boolean isEmpty() {
+        return Characters.length();
+    }
+
+    public void SetName(String name) {
+        this.Name = name;
+    }
+
+    public void SetPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public void SetEmail(String email) {
+        this.Email = email;
+    }
+
+    public String GetName() {
         return Name;
     }
-    public String GetEmail(){
+
+    public String GetEmail() {
         return Email;
     }
-    public String GetNCode(){
-        return Code;
+
+    public String GetPassword() {
+        return Password;
     }
 
-    public void AddCharacter(ChraracterSheet x){
+    public void AddCharacter(ChraracterSheet x) {
         Characters.add(x);
     }
-    public void AddSession(SessionScheduler x){
+
+    public void AddSession(SessionScheduler x) {
         Sessions.add(x);
     }
 }
-
