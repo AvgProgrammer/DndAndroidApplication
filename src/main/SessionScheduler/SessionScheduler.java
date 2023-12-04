7fa644609acf3f3e38ea8210d0c7321295848e4b
NIKOLAOS MAGKOULIAS GAVALAS <p3190105@aueb.gr>
2023-12-04 21:22:05 +0200

    Update SessionScheduler implementation and tests

--- src/main/SessionScheduler/SessionScheduler.java
+++ src/main/SessionScheduler/SessionScheduler.java
@@ -1,4 +1,4 @@
-package java.general;
+package SessionScheduler;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
--- src/test/SessionScheduler/SessionSchedulerTest.java
+++ src/test/SessionScheduler/SessionSchedulerTest.java
@@ -1,9 +1,11 @@
+package SessionScheduler;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.general.SessionScheduler;
+import SessionScheduler.SessionScheduler;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
