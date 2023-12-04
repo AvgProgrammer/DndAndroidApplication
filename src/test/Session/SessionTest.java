8ca687786c99c456d364f37eef4a07737ba1a8ef
NIKOLAOS MAGKOULIAS GAVALAS <p3190105@aueb.gr>
2023-12-04 21:40:48 +0200

    Update Session tests

--- src/test/Session/SessionTest.java
+++ src/test/Session/SessionTest.java
@@ -2,12 +2,15 @@ package Session;
 
 import org.junit.jupiter.api.Test;
 
+import CharacterSheet.CharacterSheet;
 import Dice.DiceRoll;
 import Session.Session;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
 
 class SessionTest {
     Session session;
@@ -44,6 +47,50 @@ class SessionTest {
     public void setID() {
         session.setID(1234);
         assertEquals(1234, session.getID());
+    }
+
+    @Test
+    public void getCharacterSheet() {
+        CharacterSheet c = new CharacterSheet();
+        Session tempSession = new Session(c);
+        assertEquals(c, tempSession.getCharacterSheet());
+    }
+
+    @Test
+    public void setCharacterSheet() {
+        CharacterSheet c = new CharacterSheet();
+        session.setCharacterSheet(c);
+        assertEquals(c, session.getCharacterSheet());
+    }
+
+    @Test
+    public void getDiceRolls() {
+        DiceRoll roll1 = new DiceRoll(1);
+        DiceRoll roll2 = new DiceRoll();
+        DiceRoll roll3 = new DiceRoll(1, "Wisdom check");
+
+        session.addDiceRoll(roll1);
+        session.addDiceRoll(roll2);
+        session.addDiceRoll(roll3);
+
+        ArrayList<DiceRoll> rolls = new ArrayList<DiceRoll>();
+        rolls.add(roll1);
+        rolls.add(roll2);
+        rolls.add(roll3);
+
+        assertArrayEquals(rolls.toArray(), session.getDiceRolls().toArray());
+    }
+
+    @Test
+    public void setDiceRolls() {
+        ArrayList<DiceRoll> rolls = new ArrayList<DiceRoll>();
+        rolls.add(new DiceRoll(-1));
+        rolls.add(new DiceRoll());
+        rolls.add(new DiceRoll(2, "Charisma check"));
+
+        session.setDiceRolls(rolls);
+
+        assertArrayEquals(rolls.toArray(), session.getDiceRolls().toArray());
     }
 
 }
