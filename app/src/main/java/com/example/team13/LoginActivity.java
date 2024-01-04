8047326e33e482a753b300ab9eac86632c422d3d
AvgProgrammer <katsaros.lampros01@gmail.com>
2024-01-04 23:04:54 +0200

    Update the login and SingUp to Get the user Input

--- app/src/main/java/com/example/team13/LoginActivity.java
+++ app/src/main/java/com/example/team13/LoginActivity.java
@@ -6,22 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 import com.example.Player.Player;
 
 public class LoginActivity extends AppCompatActivity {
 
     protected Button SingIn,SingUp;
+    protected EditText FindEmail,FindPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        String email=FindEmail.getText().toString();
+        String pass=FindPassword.getText().toString();
         initViews();
         SingUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Player player = new Player("PlayerName", "email@example.com", "password123");
                 Intent intent=new Intent(LoginActivity.this, SingUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        SingIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Player player = new Player("PlayerName", "email@example.com", "password123");
+                Intent intent=new Intent(LoginActivity.this, MainActivity.class);
                 intent.putExtra("Player", player);
                 startActivity(intent);
             }
@@ -30,5 +42,7 @@ public class LoginActivity extends AppCompatActivity {
     private void initViews(){
         SingIn = findViewById(R.id.SingIn);
         SingUp = findViewById(R.id.SingUp);
+        FindEmail= findViewById(R.id.RegisterName);
+        FindPassword= findViewById(R.id.RegisterPassword);
     }
 }
--- app/src/main/java/com/example/team13/SingUpActivity.java
+++ app/src/main/java/com/example/team13/SingUpActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.Player.Player;
 
 public class SingUpActivity extends AppCompatActivity {
 
     protected Button SingUpBut,Login;
+    protected EditText RegisterName,RegisterEmail,RegisterPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +24,7 @@ public class SingUpActivity extends AppCompatActivity {
         SingUpBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Player player = new Player("PlayerName", "email@example.com", "password123");
+                Player player = new Player(RegisterName.getText().toString(), RegisterEmail.getText().toString(), RegisterPassword.getText().toString());
                 Intent intent=new Intent(SingUpActivity.this, MainActivity.class);
                 intent.putExtra("Player", player);
                 startActivity(intent);
@@ -39,5 +41,8 @@ public class SingUpActivity extends AppCompatActivity {
 
         SingUpBut = findViewById(R.id.SingUpBut);
         Login = findViewById(R.id.Login);
+        RegisterName=findViewById(R.id.RegisterName);
+        RegisterEmail=findViewById(R.id.RegisterEmail);
+        RegisterPassword=findViewById(R.id.RegisterPassword);
     }
 }
