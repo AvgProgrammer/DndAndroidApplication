58597cac55013177e84ed50b4444ca6f766d6fee
AvgProgrammer <katsaros.lampros01@gmail.com>
2024-01-06 14:21:08 +0200

    Update the CharacterListActitiys Recylce view for character modification

--- app/src/main/java/com/example/team13/CharacterRecViewAdapter.java
+++ app/src/main/java/com/example/team13/CharacterRecViewAdapter.java
@@ -1,11 +1,14 @@
 package com.example.team13;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.CharacterSheet.CharacterSheet;
@@ -15,6 +18,12 @@ import java.util.ArrayList;
 public class CharacterRecViewAdapter extends RecyclerView.Adapter<CharacterRecViewAdapter.ViewHolder1>{
 
     private ArrayList<CharacterSheet> characters=new ArrayList<CharacterSheet>();
+    private Context mContext;
+
+    public CharacterRecViewAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
     public CharacterRecViewAdapter(){
 
     }
@@ -29,7 +38,14 @@ public class CharacterRecViewAdapter extends RecyclerView.Adapter<CharacterRecVi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder1 holder, int position) {
-            holder.CharName.setText(characters.get(position).getName());
+            holder.CharName.setText((position+1)+")"+characters.get(position).getName());
+            holder.Parent1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent=new Intent(mContext, PreviewSheet.class);
+                    mContext.startActivity(intent);
+                }
+            });
     }
 
     @Override
@@ -44,10 +60,13 @@ public class CharacterRecViewAdapter extends RecyclerView.Adapter<CharacterRecVi
 
     public class ViewHolder1 extends RecyclerView.ViewHolder{
 
+        private CardView Parent1;
         private TextView CharName;
+        private TextView DescChar;
         public ViewHolder1(@NonNull View itemView) {
             super(itemView);
             CharName= itemView.findViewById(R.id.CharName);
+            Parent1= itemView.findViewById(R.id.Parent1);
         }
     }
 }
---
+++ app/src/main/java/com/example/team13/PreviewSheet.java
@@ -0,0 +1,13 @@
+package com.example.team13;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreviewSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_sheet);
+    }
+}
